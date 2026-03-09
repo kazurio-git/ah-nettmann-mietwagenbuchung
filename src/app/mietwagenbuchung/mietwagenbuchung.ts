00ffbd02f27cbca76cc5cfaa7c9ddd28240e4325
Robin Wong <robinjkwong@gmail.com>
2026-03-09 13:52:45 +0100

    Update Mietwagenbuchung layout and styles for improved UI

--- src/app/mietwagenbuchung/mietwagenbuchung.ts
+++ src/app/mietwagenbuchung/mietwagenbuchung.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-mietwagenbuchung',
   imports: [
     FormsModule,
+    MatButton,
   ],
   templateUrl: './mietwagenbuchung.html',
   styleUrl: './mietwagenbuchung.css',
